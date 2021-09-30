1399972e351d2d51dbb3612600a519a8590ad392
kms69 <kavehmohammadi139@gmail.com>
2021-09-30 14:47:42 +0330

    products tabled added

---
+++ database/migrations/2021_09_30_104956_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->bigInteger('price');
+            $table->timestamp('updated_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
