04a74919fec12d17e4f525634dedd556a9479fa5
kms69 <kavehmohammadi139@gmail.com>
2021-09-30 15:00:48 +0330

    orders tabled added

---
+++ database/migrations/2021_09_30_112307_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', ['waiting', 'preparation', 'ready','delivered']);
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
