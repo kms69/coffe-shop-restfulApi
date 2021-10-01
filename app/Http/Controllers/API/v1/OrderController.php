994a23054c9d82f3d64efa33722e4924970d9daf
kms69 <kavehmohammadi139@gmail.com>
2021-10-01 09:57:02 +0330

    store order function addded

--- app/Http/Controllers/API/v1/OrderController.php
+++ app/Http/Controllers/API/v1/OrderController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreOrderRequest;
 use App\Models\Order;
+use App\Models\Order_Detail;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -11,6 +13,16 @@ class OrderController extends Controller
     public function index()
     {
          Order::paginate(20);
+
+    }
+    public function store(StoreOrderRequest $request)
+    {
+         $attribute=$request->validated();
+        Order::create($attribute);
+        Order_Detail::create($attribute);
+        return response()->json([
+            'message' => 'order has been created'
+        ], 201);
 
     }
 }
